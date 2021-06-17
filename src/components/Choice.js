4996b4665c1c81c2b5e799f6f7ff9aab3cf65d63
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-06-17 15:49:26 -0400

    added container and improved markup

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function Choice({image, name}) {
+export default function Choice({image, name, setChoice, startGame}) {
+    const handleClick = () => {
+        setChoice(name);
+        startGame();
+    }
     return (
         <div className={`choice-border ${name}`}>
-            <div className={`choice`} >
+            <div
+                onClick={() => handleClick()} 
+                className={`choice`} >
                 <img src={image} />
             </div>
         </div>
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import rock from "../images/icon-rock.svg";
 import paper from "../images/icon-paper.svg";
 import scissors from "../images/icon-scissors.svg";
 import Choice from "./Choice";
 
 export default function Game({setScore}) {
+    const [gameStart, setGameStart] = useState(false);
+    const [playerChoice, setPlayerChoice] = useState("");
+    useEffect(() => {
+        console.log(playerChoice);
+    }, [playerChoice]);
     return (
-        <section className="choice-container">
-            <Choice image={rock} name={"rock"} />
-            <Choice image={scissors} name={"scissors"} />
-            <Choice image={paper}  name={"paper"} />
+        <section className="game-container">
+            <div className="choice-container">
+                <Choice 
+                    image={rock} 
+                    name={"rock"} 
+                    setChoice={setPlayerChoice} 
+                    startGame={() => setGameStart(true)} />
+                <Choice 
+                    image={scissors} 
+                    name={"scissors"} 
+                    setChoice={setPlayerChoice} 
+                    startGame={() => setGameStart(true)} />
+                <Choice 
+                    image={paper}  
+                    name={"paper"} 
+                    setChoice={setPlayerChoice} 
+                    startGame={() => setGameStart(true)} />
+            </div>
         </section>
     )
 }
