75e8747449a653f632db76bced5cd65019950e22
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-06-18 12:01:47 -0400

    restyled the modal

--- src/components/modals/RulesModal.js
+++ src/components/modals/RulesModal.js
@@ -7,10 +7,13 @@ export default function RulesModal({visible, exit}) {
             style={{display: visible ? "flex" : "none"}} 
             className="backdrop">
             <div className="rules-modal">
-                <h2 className="rules-title">RULES</h2>
-                <button 
-                    onClick={() => exit()}
-                    className="exit-button">X</button>
+                <div className="rules-modal-top">
+                    <h2 className="rules-title">RULES</h2>
+                    <button 
+                        onClick={() => exit()}
+                        className="exit-button">X</button>
+                    
+                </div>
                 <img src={rulesImage} />
             </div>
         </div>
